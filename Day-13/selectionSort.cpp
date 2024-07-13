fd6cef915548b9221bcc182e1c644149b57000d9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-13 22:47:08 +0530

    feat: Add selection sort algorithm implementation

---
+++ Day-13/selectionSort.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void selectionSort(vector<int>& nums){
+    int n = nums.size();
+
+    for(int i = 0; i<n-1; i++){
+        int minIndex = i;
+        for(int j = i+1; j<n; j++){
+            if(nums[j] < nums[minIndex]){
+                minIndex = j;
+            }
+        }
+        // swap
+        swap(nums[i],nums[minIndex]);
+    }
+}
+
+void printArray(vector<int>& nums){
+    for(int i = 0; i<nums.size(); i++){
+        cout<<nums[i]<<" ";
+    }
+}
+
+int main()
+{
+    vector<int>nums{5,4,3,2,1};
+    selectionSort(nums);
+    printArray(nums);
+}
