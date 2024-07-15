3ca9684f5d5ea93c3d88fb4ebe6177c04d3d2fb6
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-15 23:33:27 +0530

    Day-15

---
+++ Day-15/prefixSum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+vector<int> prefixSum(vector<int>& nums){
+    int n = nums.size();
+    vector<int>pf(n);
+
+    for(int i = 0; i<n; i++){
+        int sum = 0;
+        for(int j = 0; j <= i;  j++){
+            sum = sum+nums[j];
+        }
+        pf[i] = sum;
+    }
+    return pf;
+}
+
+
+int main()
+{
+    vector<int>nums{4,1,5,-2,7};
+
+    vector<int>prefixArray = prefixSum(nums);
+
+    for(int i = 0; i<prefixArray.size(); i++){
+        cout<<prefixArray[i]<<" ";
+    }
+}
