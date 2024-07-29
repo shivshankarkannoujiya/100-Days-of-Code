8977374d75ec1dec6516e1b316aa83b7233da959
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-07-29 13:30:22 +0530

    chore: Add optimal solution for checking if two strings are anagrams

--- Day-28/validAnagram.cpp
+++ Day-28/validAnagram.cpp
@@ -10,12 +10,38 @@ bool isAnagramBruteForece(string s, string t){
     (s == t) ? true : false;
 }
 
+bool isAnagramOptimal(string s, string t){
+
+    int frequencyTable[256] = {0};
+
+    for(int i = 0; i<s.size(); i++){
+        char ch = s[i];
+        frequencyTable[ch]++;
+    }
+
+    for(int i = 0; i<t.size(); i++){
+        char ch = t[i];
+        frequencyTable[ch]--;
+    }
+
+    for(int i = 0; i<256; i++){
+        if (frequencyTable[i] != 0)
+        {
+            return false;
+        }
+        
+    }
+
+    return true;
+}
+
 
 int main(){
 
     string s = "anagram";
     string t = "nagaram";
-    bool ans = isAnagramBruteForece(s,t);
+    // bool ans = isAnagramBruteForece(s,t);
+    bool ans = isAnagramOptimal(s,t);
     
     if (ans == true)
     {
