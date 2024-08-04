8e29f79eafb188deae571f38f14c3cbc47bee2f0
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-08-04 17:53:59 +0530

    add code for two sum

---
+++ Day-35/pairSum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> twoSumBruteForce(vector<int>& arr, int target){
+
+    vector<int>ans;
+    for(int i = 0; i<arr.size(); i++){
+        for(int j = i+1; j<arr.size(); j++){
+            if (arr[i]+arr[j] == target)
+            {
+                ans.push_back(i);
+                ans.push_back(j);
+            } 
+        }
+    }
+
+    return ans;
+}
+
+
+vector<int> twoSumOptimal(vector<int>& nums, int target){
+    
+}
+
+int main()
+{
+    vector<int>arr{2,7,11,15};
+    int target = 9;
+    vector<int>ans = twoSumBruteForce(arr,target);
+
+    // printing answer
+    for(auto it : ans){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+}
