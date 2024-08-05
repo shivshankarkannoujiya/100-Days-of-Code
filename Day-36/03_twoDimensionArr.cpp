e54cf17d743f7098f4067e397a40c78b25f8ad81
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-08-05 14:04:38 +0530

    2D Arr

--- Day-36/03_twoDimensionArr.cpp
+++ Day-36/03_twoDimensionArr.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 
+void printTwoDimensionalArr(int arr[][4], int row, int col ){
 
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 
 int main()
 {
@@ -10,15 +18,15 @@ int main()
 
     // TODO: Array Initialization
     
-    int arr[2][4] = {
-        {1,2,3,4},
-        {5,6,7,8}
-    };
+    // int arr[2][4] = {
+    //     {1,2,3,4},
+    //     {5,6,7,8}
+    // };
 
-    int arr1[][4] = {
-        {1,2,3,4},
-        {5,6,7,9}
-    };
+    // int arr1[][4] = {
+    //     {1,2,3,4},
+    //     {5,6,7,9}
+    // };
 
     //TODO: Accessing Elements
     // std::cout<<arr1[0][2]<<endl;
@@ -41,13 +49,39 @@ int main()
         {9,10,11,12}
     };
 
-    int row = 3;
+    // int row = 3;
+    // int col = 4;
+    // for(int i = 0; i<col; i++){
+    //     for(int j = 0; j<row; j++){
+    //         cout<<arr2[j][i]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    // TODO: Take Input Row and Col Wise
+    int arr[3][4];
+    int row = 3; 
     int col = 4;
-    for(int i = 0; i<col; i++){
-        for(int j = 0; j<row; j++){
-            cout<<arr2[j][i]<<" ";
+
+    //! Row Wise 
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            cout<<"Enter Element at "<<"["<<i<<"]"<<" "<<"["<<j<<"]"<<" : ";
+            cin>>arr[i][j];
         }
         cout<<endl;
     }
+
+    //! Col wise
+    for(int i = 0; i<col; i++){
+        for(int j = 0; j<row; j++){
+            cout<<"Enter Element at "<<"["<<j<<"]"<<" "<<"["<<i<<"]"<<" : ";
+            cin>>arr[j][i];
+        }
+        cout<<endl;
+    }
+
+    printTwoDimensionalArr(arr,row,col);
     
 }
