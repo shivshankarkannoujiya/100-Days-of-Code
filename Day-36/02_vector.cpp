afc2b54d2522350401b545fea8afd291ab51dd90
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-08-05 11:09:28 +0530

    vector

---
+++ Day-36/02_vector.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+
+int main()
+{
+    //TODO: vector Initialization
+    // vector<int>v1;
+    // vector<int>v2(5,1);
+    // vector<int>v3{1,1,1,1,1};
+    // vector<int>v4 = {1,1,1,1,1};
+
+    //TODO: Create a Vector using existing Vector
+    // vector<int>arr{1,2,3,4,5};
+    // vector<int>vrr(arr);
+
+    //TODO: Get the first and last element of Vector
+    // vector<int>arr{1,2,3,4,5};
+    // cout<<"First Element : "<<arr[0]<<endl;
+    // cout<<"First Element : "<<arr.front()<<endl;
+    // cout<<"Last  Element : "<<arr[arr.size()-1]<<endl;
+    // cout<<"Last  Element : "<<arr.back()<<endl;
+
+    //TODO: Print Vector using forEach Loop 
+    // vector<int>v = {1,2,3,4,5};
+    // for(auto it: v){
+    //     cout<<it<<" ";
+    // }
+    // cout<<endl;
+
+}
