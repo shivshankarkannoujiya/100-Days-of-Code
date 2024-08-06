8721d54df230f627cd3cbe307497134730453d0a
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2024-08-06 16:23:52 +0530

    add code for transpose matrix

---
+++ Day-37/03_transposeMatrix.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void transposeMatrix(int arr[][4], int row, int col){
+
+    for(int i = 0; i<row; i++){
+        for(int j = i+1; j<col; j++){
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+}
+
+void printArray(int arr[][4], int row, int col){
+    for(int i = 0; i<row; i++){
+        for(int j = 0; j<col; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int arr[][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+
+    int row = 4;
+    int col = 4;
+
+    cout<<"Printing Arr Before transpose : "<<endl;
+    printArray(arr,row,col);
+    cout<<"Printing Arr After transpose : "<<endl;
+    transposeMatrix(arr,row,col);
+    printArray(arr,row,col);
+}
